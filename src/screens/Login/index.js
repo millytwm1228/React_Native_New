004e7099900013e701743ef6ae6a8b7420253b32
milly <millytwm1228@gmail.com>
2021-06-15 15:15:50 +0800

    login component

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,36 +1,11 @@
 import React from 'react';
-import { View, Text, TextInput } from 'react-native';
-import Container from '../../components/common/Container/index';
-import Input from '../../components/common/Input/index';
-import CustomButton from '../../components/common/CustomButton/index';
+import LoginComponent from '../../components/Login/index';
 
 const Login = () => {
     const [value, onChangeText] = React.useState('');
   
     return (
-    <Container style={{marginTop: 50}}>
-        <Input
-         label="Username"
-         onChangeText={(text) => onChangeText(text)}
-         value={value}
-        //  icon={<Text>HIDE</Text>}
-         iconPosition="right"
-         error={'This field is required'}
-        />
-
-        <Input
-         label="Password"
-         onChangeText={(text) => onChangeText(text)}
-         value={value}
-         icon={<Text>HIDE</Text>}
-         iconPosition="right"
-        />
-        <CustomButton secondary title="Submit" loading={true}  disabled={true}></CustomButton>
-        <CustomButton secondary loading={true} title="ClickMe" ></CustomButton>
-        <CustomButton primary title="Submit" loading={true}  disabled={true}></CustomButton>
-        <CustomButton danger title="Submit" ></CustomButton>
-
-    </Container>
+     <LoginComponent />
     );
 }
 
