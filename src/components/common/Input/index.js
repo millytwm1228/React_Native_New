1236ad257d6471e712d8e8be60c60037194b2bbb
milly <millytwm1228@gmail.com>
2021-06-24 14:51:40 +0800

    3:17:52 - Login a user 3:34:02 - Authentication Flow 3:39:42 - Custom navigation drawer 3:57:13 -  Alert component 4:00:18 - Logout a user 4:06:13 - Toggle password visibility 4:09:27 - Route Params, Callbacks, messages, and more. 4:17:34 - Setting up react native vector icons 4:26:45 - Custom icon component 4:32:32 - Custom Modal 4:53:29 - Data Fetching

--- src/components/common/Input/index.js
+++ src/components/common/Input/index.js
@@ -1,63 +1,70 @@
-import React from 'react'
-import { View, Text, TextInput } from 'react-native'
-import { color } from 'react-native-reanimated'
-import { Colors } from 'react-native/Libraries/NewAppScreen'
-import colors from '../../../assets/theme/colors'
-import styles from './styles'
+import React from 'react';
+import {View, Text, TextInput} from 'react-native';
+import colors from '../../../assets/theme/colors';
+import styles from './styles';
 
-const Input = ({onChangeText, iconPosition, icon, style, value, label, error,
- ...props
+const Input = ({
+  onChangeText,
+  iconPosition,
+  icon,
+  style,
+  value,
+  label,
+  error,
+  ...props
 }) => {
-    const [focused, setFocused] = React.useState(false);
+  const [focused, setFocused] = React.useState(false);
 
-    const getFlexDirection=()=>{
-        if(icon && iconPosition){
-            if(iconPosition === 'left'){
-                return "row";
-            }else if(iconPosition ==="right"){
-                return "row-reverse";
-            }
-        }
+  const getFlexDirection = () => {
+    if (icon && iconPosition) {
+      if (iconPosition === 'left') {
+        return 'row';
+      } else if (iconPosition === 'right') {
+        return 'row-reverse';
+      }
+    }
+  };
+
+  const getBorderColor = () => {
+    if (error) {
+      return colors.danger;
     }
 
-    const getBorderColor=()=>{
-        if(focused){
-            return colors.primary;
-        }
-        if(error){
-            return colors.danger;
-        }else{
-            return colors.grey;
-        }
+    if (focused) {
+      return colors.primary;
+    } else {
+      return colors.grey;
     }
-    
-    return (
-        <View style={styles.inputContainer}>
-         {label && <Text>{label}</Text>}
+  };
+  return (
+    <View style={styles.inputContainer}>
+      {label && <Text>{label}</Text>}
 
-         <View style={[styles.wrapper, 
-            {alignItems: icon ? "center" : "baseline"},
-            {borderColor: getBorderColor(), flexDirection: getFlexDirection()}]}>
-            <View>{icon && icon}</View>
+      <View
+        style={[
+          styles.wrapper,
+          {alignItems: icon ? 'center' : 'baseline'},
+          {borderColor: getBorderColor(), flexDirection: getFlexDirection()},
+        ]}>
+        <View>{icon && icon}</View>
 
-         <TextInput
-           style={[styles.textInput, style]}
-           onChangeText={onChangeText}
-           value={value}
-           onFocus={()=>{
-               setFocused(true);
-           }}
+        <TextInput
+          style={[styles.textInput, style]}
+          onChangeText={onChangeText}
+          value={value}
+          onFocus={() => {
+            setFocused(true);
+          }}
+          onBlur={() => {
+            setFocused(false);
+          }}
+          {...props}
+        />
+      </View>
 
-           onBlur={()=>{
-               setFocused(false);
-           }}
-           {...props}
-         />
-         </View>
+      {error && <Text style={styles.error}>{error}</Text>}
+    </View>
+  );
+};
 
-         {error && <Text style={styles.error}>{error}</Text>}
-        </View>
-    )
-}
-
-export default Input
+export default Input;
